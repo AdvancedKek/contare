a3443f3234410e24623f78a45d26dffc3b136b8e
iUsup <yusub1988@gmail.com>
2024-07-10 11:22:06 +0300

    feat: Edd domain task models

---
+++ domain/src/main/java/io/github/advancedkek/contare/domain/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package io.github.advancedkek.contare.domain.exception;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
